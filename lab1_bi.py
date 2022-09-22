55780bf0b7cb5a57f9156cbc5332324709f03f1b
Summit To <sokolovila543@gmail.com>
2022-09-22 14:17:06 +0300

    первый коммит

---
+++ lab1_bi.py
@@ -0,0 +1,100 @@
+#
+#from readline import append_history_file
+import sys
+import math
+
+def get_coef(index, prompt):
+    '''
+    Читаем коэффициент из командной строки или вводим с клавиатуры
+
+    Args:
+        index (int): Номер параметра в командной строке
+        prompt (str): Приглашение для ввода коэффицента
+
+    Returns:
+        float: Коэффициент квадратного уравнения
+    '''
+    try:
+        # Пробуем прочитать коэффициент из командной строки
+        coef_str = sys.argv[index]
+    except:
+        # Вводим с клавиатуры
+        print(prompt)
+        coef_str = input()
+    # Переводим строку в действительное число
+    coef = float(coef_str)
+    return coef
+
+
+def get_roots(a, b, c):
+    '''
+    Вычисление корней квадратного уравнения
+
+    Args:
+        a (float): коэффициент А
+        b (float): коэффициент B
+        c (float): коэффициент C
+
+    Returns:
+        list[float]: Список корней
+    '''
+    result = []
+    D = b*b - 4*a*c
+    if D == 0.0:
+        root = -b / (2.0*a)
+        result.append(root)
+    elif D > 0.0:
+        sqD = math.sqrt(D)
+        root1 = (-b + sqD) / (2.0*a)
+        root2 = (-b - sqD) / (2.0*a)
+        result.append(root1)
+        result.append(root2)
+    return result
+
+
+def main():
+    '''
+    Основная функция
+    '''
+    print('Уравнение типа Ax^4+Bx^2+C=0')
+    a = get_coef(1, 'Введите коэффициент А:')
+    b = get_coef(2, 'Введите коэффициент B:')
+    c = get_coef(3, 'Введите коэффициент C:')
+    # Вычисление корней
+    roots = get_roots(a,b,c)
+    bi_roots=get_bi_roots(roots)
+    # Вывод корней
+    # print(len(bi_roots))
+
+    # for i in bi_roots:
+    #     print(i)
+    len_bi_roots=len(bi_roots)
+
+    if len_bi_roots == 0:
+        print('Нет корней')
+    
+    elif len_bi_roots == 2:
+        print('Два корня: {} и {}'.format(bi_roots[0], bi_roots[1]))
+    
+
+    elif len_bi_roots == 4:
+        print('Четыре корня: {} и {},{} и {}'.format(bi_roots[0], bi_roots[1],bi_roots[2],bi_roots[3]))
+    
+
+def get_bi_roots(roots):
+    '''
+    Вычисляем корни биквадратного уравения, входные данные типа list
+    '''
+    result=[]
+    for i in roots:
+        if i>0.0:
+            result.append((-1)*i**(1/2))
+            result.append(i**(1/2))
+    
+    return result
+# Если сценарий запущен из командной строки
+if __name__ == "__main__":
+    main()
+
+# Пример запуска
+# qr.py 1 0 -4
